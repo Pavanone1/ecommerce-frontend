26f0597a5198aea889e44161c29fbd94bb919a4b
Pavanone1 <balsarafpavan4@gmail.com>
2026-02-05 00:54:45 +0530

    Describe what you changed

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
-document.addEventListener("DOMContentLoaded", () => {
-  console.log("E-commerce website loaded successfully!");
+const heroCard = document.querySelector(".hero-card");
+
+window.addEventListener("scroll", () => {
+  const position = heroCard.getBoundingClientRect().top;
+  const screenHeight = window.innerHeight;
+
+  if (position < screenHeight - 100) {
+    heroCard.style.opacity = "1";
+  }
 });
